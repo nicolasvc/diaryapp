05ef814907bddc8cc8f4793ef8b8ec89d6b89c05
Morfeo <nicovc20@gmail.com>
2023-09-14 09:33:16 -0500

    add librarys and theme

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,16 +1,20 @@
 plugins {
     id("com.android.application")
+    id ("kotlin-kapt")
     id("org.jetbrains.kotlin.android")
+    id("com.google.devtools.ksp")
+    id("com.google.dagger.hilt.android")
+    id ("io.realm.kotlin")
 }
 
 android {
     namespace = "com.morfeo.diaryapp"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.morfeo.diaryapp"
         minSdk = 24
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
@@ -30,11 +34,11 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
     kotlinOptions {
-        jvmTarget = "1.8"
+        jvmTarget = "17"
     }
     buildFeatures {
         compose = true
@@ -51,9 +55,9 @@ android {
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.9.0")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.1")
-    implementation("androidx.activity:activity-compose:1.7.0")
+    implementation("androidx.core:core-ktx:1.12.0")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
+    implementation("androidx.activity:activity-compose:1.7.2")
     implementation(platform("androidx.compose:compose-bom:2023.03.00"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
@@ -66,4 +70,59 @@ dependencies {
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
+
+
+    // Compose Navigation
+    implementation ("androidx.navigation:navigation-compose:2.7.2")
+
+    // Firebase
+    implementation ("com.google.firebase:firebase-auth-ktx:22.1.1")
+    implementation ("com.google.firebase:firebase-storage-ktx:20.2.1")
+
+    // Room components
+    implementation ("androidx.room:room-runtime:2.5.2")
+    ksp ("androidx.room:room-compiler:2.5.2")
+    implementation ("androidx.room:room-ktx:2.5.2")
+
+    // Runtime Compose
+    implementation ("androidx.lifecycle:lifecycle-runtime-compose:2.6.2")
+
+    // Splash API
+    implementation ("androidx.core:core-splashscreen:1.0.1")
+
+    // Mongo DB Realm
+    implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
+    implementation ("io.realm.kotlin:library-sync:1.10.0")
+
+    // Dagger Hilt
+    implementation ("com.google.dagger:hilt-android:2.48")
+    kapt ("com.google.dagger:hilt-compiler:2.48")
+    implementation ("androidx.hilt:hilt-navigation-compose:1.0.0")
+
+    // Coil
+    implementation ("io.coil-kt:coil-compose:2.4.0")
+
+    // Pager - Accompanist
+    implementation ("com.google.accompanist:accompanist-pager:0.27.0")
+
+    // Date-Time Picker
+    implementation ("com.maxkeppeler.sheets-compose-dialogs:core:1.0.2")
+
+    // CALENDAR
+    implementation( "com.maxkeppeler.sheets-compose-dialogs:calendar:1.0.2")
+
+    // CLOCK
+    implementation( "com.maxkeppeler.sheets-compose-dialogs:clock:1.0.2")
+
+    // Message Bar Compose
+    implementation ("com.github.stevdza-san:MessageBarCompose:1.0.5")
+
+    // One-Tap Compose
+    implementation ("com.github.stevdza-san:OneTapCompose:1.0.3")
+
+
+    // Desugar JDK
+    coreLibraryDesugaring ("com.android.tools:desugar_jdk_libs:1.2.0")
+
+
 }
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,17 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
+buildscript {
+    repositories {
+        google()
+    }
+    dependencies {
+       //classpath("com.google.gms:google-services:4.3.15")
+    }
+}
 plugins {
     id("com.android.application") version "8.1.1" apply false
     id("org.jetbrains.kotlin.android") version "1.8.10" apply false
+    id("com.google.devtools.ksp") version "1.8.10-1.0.9" apply false
+    id("com.google.dagger.hilt.android") version "2.48" apply false
+    id("io.realm.kotlin") version "1.11.1" apply false
+
 }
